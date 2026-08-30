8e881ef47e70b69e56aed3d339d6f7e0807f0883
PukoDev <puko.workspace@gmail.com>
2026-08-30 14:58:19 +0800

    update

--- src/common/macros.h
+++ src/common/macros.h
@@ -17,6 +17,7 @@
 #pragma once
 
 #include "common/version.h"
+#include <godot_cpp/classes/translation_server.hpp>
 
 #define OACCEL_KEY(mask,key) (Key(static_cast<int>(mask) | static_cast<int>(key)))
 
@@ -89,3 +90,16 @@
     (source)->grab_focus();
 
 #define SNAME(x) StringName((x))
+
+
+inline godot::String orchestrator_tr(const godot::String &p_message, const godot::StringName &p_context = "") {
+    godot::TranslationServer *ts = godot::TranslationServer::get_singleton();
+    if (!ts) {
+        return p_message;
+    }
+    // 指定外掛的 Domain 進行翻譯
+    return ts->translate(p_message, p_context, "godot_orchestrator");
+}
+
+// 供 C++ 原始碼呼叫的巨集
+#define OTR(m_text) orchestrator_tr(m_text)
